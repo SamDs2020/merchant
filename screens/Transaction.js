62b72ef6e4f6ec595c86c1d95d1978097778843a
Oshianor <abundanceoshianor@gmail.com>
2019-11-27 22:07:12 +0100

    done

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -2,8 +2,39 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text, SafeAreaView } from 'react-native'
 import { Layout, Button, Avatar, Input } from 'react-native-ui-kitten'
+import axios from 'axios'
+
+
 
 export default class Transaction extends Component {
+  state = {
+    value: '',
+  };
+
+  onChangeText = (value) => {
+    this.setState({ value });
+  };
+
+
+  handleAward = async () => {
+    try {
+      const transactionId = this.props.navigation.getParam(
+        'transactionId',
+        '5dc8a76c4f40fc00176e1295'
+      );
+      const tran = await axios.post('/transaction/transaction', {
+        refId: this.state.value,
+        transactionId
+      });
+
+      console.log('tran', tran);
+      this.props.navigation.navigate('Home')
+    } catch (error) {
+      console.log("error", error);
+      console.log('error', error.response)
+    }
+  }
+
 	render() {
 		const discount = this.props.navigation.getParam('discount', 10)
 		const transactionId = this.props.navigation.getParam(
@@ -20,7 +51,18 @@ export default class Transaction extends Component {
           <Text style={{ fontSize: 20 }}>Discount</Text>
         </View>
 
+        <Input
+          style={styles.input}
+          value={this.state.value}
+          onChangeText={this.onChangeText}
+          placeholder="Enter Transaction ID"
+        />
+        <Button style={styles.button} onPress={this.handleAward} size="large">
+          Award Gem Points
+        </Button>
         <View style={styles.mid}>
+          <Text style={{ margin: 20 }}>OR</Text>
+
           <Button
             style={styles.button}
             onPress={() =>
@@ -54,6 +96,10 @@ const styles = StyleSheet.create({
   mid: {
     // flex: 0.3,
     marginTop: 30
+  },
+  input: {
+    flex: 1,
+    marginHorizontal: 4
   }
 })
 
